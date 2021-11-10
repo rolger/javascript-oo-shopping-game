cff55a2d3160759861c8fc1a3a536ca0d9a8d287
rolger <roland.germ@gmail.com>
2021-11-10 15:54:20 +0100

    feature: implement module3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -213,10 +213,16 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(product) {
+        return id === product.id;
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => { 
+    return Math.floor(Math.random() * 19) + 1; 
+};
 
 
 const getProduct = (prodList, pId) => {
